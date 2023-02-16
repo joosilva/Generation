6752ef7b420dad71842d2e35e821e34ab570398a
joosilva <joaovictormendessilva1@gmail.com>
2023-02-16 14:18:52 -0300

    Conteúdo Java.

---
+++ aulaJava/src/estruturaDeDados/exercicio1.java
@@ -0,0 +1,10 @@
+package estruturaDeDados;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ aulaJava/src/vetoresEMatriz/exercicio1.java
@@ -0,0 +1,12 @@
+package vetoresEMatriz;
+
+public class exercicio1 {
+	
+	public static void main (String[] args) {
+		
+		//Primeiro enunciado:
+		
+		
+	}
+
+}
