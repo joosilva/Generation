b6c4839aabcaf58336b9be106cdbdf684e21a4db
joosilva <joaovictormendessilva1@gmail.com>
2023-02-16 14:22:44 -0300

    Olá, Mundo!

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,7 @@
 class Simple {  
-   public static void main(String args[]) {  
-      System.out.println("Olá, Mundo.");     
+	
+   public static void main(String args[]) { 
+	   
+      System.out.println("Olá, Mundo!");     
    }  
 }  
